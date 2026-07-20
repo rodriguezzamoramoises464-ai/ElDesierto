43c10118fd8e90b53284422e5821b806967052a7
Moisés1234 <rodriguezzamoramoises464@gmail.com>
2026-07-20 17:07:04 -0400

    Create supabase.js

---
+++ docs/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://adwqaomlbmdisqtjokmw.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_x8Tb0zPEWLWsPR9WboVgXA__mx6Obkl";
+
+const clienteSupabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
